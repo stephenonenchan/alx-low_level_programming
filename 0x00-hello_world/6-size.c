f0c6606c4e30e1d546e3d1cc97f5a0fa41bf8209
stephenonenchan <onenmahrez26@gmail.com>
2023-02-18 04:51:34 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,5 +10,6 @@ int main(void)
 	printf("Size of a long int: %ld byte(s)\n", sizeof(unsigned long int));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(unsigned long int));
 	printf("size of a float: %ld byte(s)\n", sizeof(unsigned long int));
+	fprintf(stderr, "anything\n");
 	return (0);
 }
