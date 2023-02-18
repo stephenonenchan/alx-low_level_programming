284c3c3d3f0573fc2fbf71db9849e0fcd8411640
stephenonenchan <onenmahrez26@gmail.com>
2023-02-18 12:48:58 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * printing lowercase numbers using putchar
+ * return: always 0.(success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+
+	while
+	(c <= 'z');
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
