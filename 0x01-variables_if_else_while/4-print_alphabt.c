4b2ad83c4a211aee3cc8e659d36e9ebeea4d9a2f
stephenonenchan <onenmahrez26@gmail.com>
2023-02-19 01:39:59 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints alpha in lower case
+ * return: Always 0
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+
+	for (lc = 'A'; lc <= 'Z'; lc++)
+	{
+		putchar(lc);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
