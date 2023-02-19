882c97339d85993274b735793c9d8fff21ba5e95
stephenonenchan <onenmahrez26@gmail.com>
2023-02-19 03:08:24 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print numbers from 0 to 10
+ * return: 0.
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+
+		putchar((n % 10) + '0');
+
+	putchar('\n');
+
+
+	return (0);
+}
