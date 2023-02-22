402ccaf718db99817d4d0077c8c5b173aff3f3f1
stephenonenchan <onenmahrez26@gmail.com>
2023-02-22 03:36:40 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,16 +14,18 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n == 0)
-	{
-		_putchar(0);
-		return (0);
-	}
-	else
+	if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
 	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
 
 	_putchar('\n');
+
+	return (0);
 }
