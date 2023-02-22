5880f399745c9138d2689824da100d467ce764d1
stephenonenchan <onenmahrez26@gmail.com>
2023-02-22 03:21:30 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - prints sign of numbers
  * @n: number to be checked for
- * return: (1) and prints ('+') if n is greater than zero
+ * Return: (1) and prints ('+') if n is greater than zero
  * 0 and prints 0 if n is zero
  * -1 and prints ('-') if n is less than zero
  */
